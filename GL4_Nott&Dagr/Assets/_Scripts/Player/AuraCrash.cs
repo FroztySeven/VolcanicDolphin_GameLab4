36f59ce18c12a3fa3a96c855ed745bbc529af504
sindrebr <sindre980@hotmail.com>
2020-01-29 22:59:01 +0100

    Implemented the aura mechanic, but it will not work properly, so i will have a look at it tomorrow

---
+++ GL4_Nott&Dagr/Assets/_Scripts/Player/AuraCrash.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class AuraCrash : MonoBehaviour
+{
+    public GameObject nott, dagr;
+    
+    private Vector3 crashForce;
+    private Rigidbody2D nottRB, dagrRB;
+
+
+    private void Start()
+    {
+        nottRB = nott.GetComponent<Rigidbody2D>();
+        dagrRB = dagr.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.tag + "  Tag");
+
+        if (other.gameObject.CompareTag("NottAura"))
+        {
+                crashForce = dagr.transform.position - nott.transform.position; // dagr then nott
+                crashForce.Normalize();
+                
+                if (nottRB.velocity.x <= 0)
+                {
+                    if (Input.GetButton("AuraPushP1"))
+                    {
+                       nottRB.AddForce(crashForce * -30, ForceMode2D.Impulse); //change this when i fix the bug
+                    }
+                }
+
+                if (crashForce.y > 0.01f)
+                {
+                    dagrRB.velocity *= -1.5f;
+                }
+                else
+                {
+                    dagrRB.velocity *= -15;
+                }
+        }
+        
+        if (other.gameObject.CompareTag("DagrAura"))
+        {
+            crashForce = nott.transform.position - dagr.transform.position; // nott then dagr
+            crashForce.Normalize();
+            
+            if (dagrRB.velocity.x <= 0)
+            {
+                if (Input.GetButton("AuraPushP2"))
+                {
+                    dagrRB.AddForce(crashForce * 6, ForceMode2D.Impulse);
+                }
+            }
+
+            if (crashForce.y > 0.01f) 
+            { 
+                nottRB.velocity *= -1.5f;
+            }
+            else
+            {
+                nottRB.velocity *= -15;
+            }
+        }
+    }
+}
