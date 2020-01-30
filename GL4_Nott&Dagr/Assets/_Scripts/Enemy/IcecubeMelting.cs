a1d2304c6072bf2c3a45e9b5c4a95197fec36fbe
FroztySeven <froztyseven@hotmail.com>
2020-01-30 01:49:01 +0100

    Added progress with keyblock & door system

---
+++ GL4_Nott&Dagr/Assets/_Scripts/Enemy/IcecubeMelting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcecubeMelting : MonoBehaviour
+{
+    public GameObject iceCube;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 
+        if (other.gameObject.name == "Player2")
+        {
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
