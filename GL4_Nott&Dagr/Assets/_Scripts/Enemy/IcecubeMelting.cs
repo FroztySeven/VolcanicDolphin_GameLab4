007482ad59ba33d13cace0c6b02749b760463788
FroztySeven <froztyseven@hotmail.com>
2020-02-02 21:32:42 +0100

    Added in an icecube key with physics

--- GL4_Nott&Dagr/Assets/_Scripts/Enemy/IcecubeMelting.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/Enemy/IcecubeMelting.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 
 public class IcecubeMelting : MonoBehaviour
 {
+    public bool isIceCubeExisting = true;
     public GameObject iceCube;
-    
+    public GameObject pickupKey;
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // 
-        if (other.gameObject.name == "Player2")
+        if (other.gameObject.CompareTag("Player"))
         {
-            transform.GetChild(1).gameObject.SetActive(false);
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+            {
+                if (!isIceCubeExisting)
+                {
+                    /*Destroy(transform.Find("Icecube").gameObject);*/
+                    gameObject.transform.Find("Icecube").gameObject.SetActive(true);
+                    isIceCubeExisting = true;
+
+                    Destroy(transform.Find("key_01").GetComponent<BoxCollider2D>());
+                    GetComponent<Rigidbody2D>().mass = 5f;
+                    /*Destroy(transform.Find("key_01").GetComponent<Rigidbody2D>());*/
+                }
+            }
+        }
+        
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                if (isIceCubeExisting)
+                {
+                    /*Destroy(transform.Find("Icecube").gameObject);*/
+                    gameObject.transform.Find("Icecube").gameObject.SetActive(false);
+                    isIceCubeExisting = false;
+
+                    gameObject.transform.Find("key_01").gameObject.AddComponent<BoxCollider2D>();
+                    GetComponent<Rigidbody2D>().mass = 110f;
+                    /*gameObject.transform.Find("key_01").gameObject.AddComponent<Rigidbody2D>();*/
+                }
+            }
         }
     }
 }
