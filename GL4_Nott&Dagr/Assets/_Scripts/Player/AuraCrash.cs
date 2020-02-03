f56f234728b71a383f69487a8725d7649a8403fa
sindrebrokke <sindre980@hotmail.com>
2020-02-03 10:51:03 +0100

    Reworked the aura crash and made the key open the door

--- GL4_Nott&Dagr/Assets/_Scripts/Player/AuraCrash.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/Player/AuraCrash.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 
@@ -11,11 +12,15 @@ public class AuraCrash : MonoBehaviour
     private Vector3 crashForce;
     private Rigidbody2D nottRB, dagrRB;
 
+    private string[] connectedControllers;
+
 
     private void Start()
     {
         nottRB = nott.GetComponent<Rigidbody2D>();
         dagrRB = dagr.GetComponent<Rigidbody2D>();
+        connectedControllers = Input.GetJoystickNames();
+        Debug.Log(connectedControllers.Length + "  Controllers");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,11 +32,23 @@ public class AuraCrash : MonoBehaviour
                 
                 if (nottRB.velocity.x <= 0)
                 {
-                    if (Input.GetButton("AuraPushP1"))
+                    if (connectedControllers.Length == 1) // if it is only one player
                     {
-                       dagrRB.AddForce(crashForce * -30, ForceMode2D.Impulse); //change this when i fix the bug
-                       Debug.Log("AuraPushP1");
+                        if (Input.GetButton("AuraPushP1"))
+                        {
+                            nottRB.AddForce(crashForce * -30, ForceMode2D.Impulse); //change this when i fix the bug
+                            Debug.Log("AuraPushP2");
+                        }
                     }
+
+                    if (connectedControllers.Length == 2) //if 2 controllers is connected.
+                    {
+                        if (Input.GetButton("AuraPushP2"))
+                        {
+                            nottRB.AddForce(crashForce * -30, ForceMode2D.Impulse);
+                        }
+                    }
+                    
                 }
 
                 if (crashForce.y > 0.01f)
@@ -41,7 +58,7 @@ public class AuraCrash : MonoBehaviour
                 else
                 {
                     dagr.GetComponent<PlayerMovementTest>().enabled = false;
-                    dagrRB.velocity *= -5;
+                    dagrRB.velocity *= -4;
                     StartCoroutine(turnOnMovement());
                 }
         }
@@ -53,10 +70,10 @@ public class AuraCrash : MonoBehaviour
             
             if (dagrRB.velocity.x <= 0)
             {
-                if (Input.GetButton("AuraPushP2"))
+                if (Input.GetButton("AuraPushP1"))
                 {
-                    nottRB.AddForce(crashForce * -30, ForceMode2D.Impulse);
-                    Debug.Log("AuraPushP2");
+                    dagrRB.AddForce(crashForce * 10, ForceMode2D.Impulse);
+                    Debug.Log("AuraPushP1");
                 }
             }
 
@@ -67,7 +84,7 @@ public class AuraCrash : MonoBehaviour
             else
             { 
                 nott.GetComponent<PlayerMovementTest>().enabled = false;
-                nottRB.velocity *= -5;
+                nottRB.velocity *= -4;
                 StartCoroutine(turnOnMovement());
             }
         }
@@ -75,7 +92,7 @@ public class AuraCrash : MonoBehaviour
 
     private IEnumerator turnOnMovement()
     {
-        yield return new WaitForSeconds(0.15f);
+        yield return new WaitForSeconds(0.10f);
         dagr.GetComponent<PlayerMovementTest>().enabled = true;
         nott.GetComponent<PlayerMovementTest>().enabled = true;
     }
---
+++ GL4_Nott&Dagr/Assets/_Scripts/SindreTest/KeyOpenDoor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyOpenDoor : MonoBehaviour
+{
+    public Sprite closedDoor, openDoor;
+    public GameObject door;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            door.GetComponent<SpriteRenderer>().sprite = openDoor;
+            other.gameObject.SetActive(false);
+        }
+    }
+    
+}
