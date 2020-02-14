75b5a0b2e3bb06155e364932898450883984d260
Xzendor <jtors2@hotmail.com>
2020-02-14 06:45:49 +0100

    Plant prefab, upload from desktop so I can work more on it at school.

---
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/GrowingPlant.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingPlant : MonoBehaviour
+{
+    public Sprite bottom, top;
+    public Sprite[] middle;
+
+    public GameObject plantPrefab;
+
+    public List<GameObject> plantList;
+
+    public int growHeight;
+    public float growSpeed;
+
+    private int growCount = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                Debug.Log("Something hit me!");
+                StartCoroutine(PlantGrow());
+                gameObject.GetComponent<SpriteRenderer>().sprite = null;
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+    }
+
+    IEnumerator PlantGrow()
+    {
+        if (growCount <= growHeight)
+        {
+            GameObject plant = Instantiate(plantPrefab, transform.position + new Vector3(0f, growCount, 0f), transform.rotation);
+            plant.transform.parent = gameObject.transform;
+            plantList.Add(plant);
+
+            if (growCount == 0)
+            {
+                plant.GetComponent<SpriteRenderer>().sprite = bottom;
+            }
+            else
+            {
+                plant.GetComponent<SpriteRenderer>().sprite = middle[Random.Range(0, middle.Length)];
+            }
+
+            if (growCount == growHeight)
+            {
+                plant.GetComponent<SpriteRenderer>().sprite = top;
+            }
+
+
+            yield return new WaitForSeconds(growSpeed);
+            growCount++;
+            StartCoroutine(PlantGrow());
+        }
+    }
+}
---
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/Plant.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                other.GetComponent<PlayerMovementTest>().isOnLadder = true;
+            }
+
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+            {
+                other.GetComponent<PlayerMovementTest>().isOnLadder = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                other.GetComponent<PlayerMovementTest>().isOnLadder = false;
+            }
+
+            if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+            {
+                other.GetComponent<PlayerMovementTest>().isOnLadder = false;
+            }
+        }
+    }
+}
