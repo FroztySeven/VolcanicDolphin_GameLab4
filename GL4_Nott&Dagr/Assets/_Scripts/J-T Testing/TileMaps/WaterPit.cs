bb0a8edf6b56ef0442a9dd09e4542de059b473c9
Xzendor <jtors2@hotmail.com>
2020-02-18 11:10:17 +0100

    Reworked water prefab, player specific trampolines, sprite atlas for tile flicker fix.

--- GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/WaterPit.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/WaterPit.cs
@@ -8,6 +8,7 @@ public class WaterPit : MonoBehaviour
 
     public int waterLength;
 
+    public bool isAnimated;
     public float animationRate;
 
     public GameObject waterPrefab;
@@ -19,6 +20,14 @@ public class WaterPit : MonoBehaviour
     public int spriteNumber = 0;
     [HideInInspector]
     public int animationFrames;
+
+    private float waterFreezeTimer = 0;
+    private bool isFreezing;
+    private int waterTileFreezeLeft, waterTileFreezeRight;
+
+    private float waterUnfreezeTimer = 0;
+    private bool isUnfreezing;
+    private int waterTileUnfreezeLeft, waterTileUnfreezeRight;
 
     private void Start()
     {
@@ -42,102 +51,181 @@ public class WaterPit : MonoBehaviour
 
     private void Update()
     {
-        if (spriteTimer < 1)
+        if (isAnimated)
         {
-            spriteTimer += Time.deltaTime * animationRate;
+            if (spriteTimer < 1)
+            {
+                spriteTimer += Time.deltaTime * animationRate;
+            }
+            if (spriteTimer >= 1)
+            {
+                spriteNumber++;
+                spriteTimer = 0;
+            }
+            if (spriteNumber == animationFrames)
+            {
+                spriteNumber = 0;
+            }
         }
-        if (spriteTimer >= 1)
+
+        if (isFreezing)
         {
-            spriteNumber++;
-            spriteTimer = 0;
+            if (waterFreezeTimer < freezeSpeed)
+            {
+                waterFreezeTimer += Time.deltaTime;
+            }
+            else
+            {
+                waterFreezeTimer = 0;
+                if (waterTileFreezeLeft >= 0)
+                {
+                    waterTiles[waterTileFreezeLeft].GetComponent<Water>().Frozen();
+                    waterTileFreezeLeft--;
+                }
+                if (waterTileFreezeRight < waterLength)
+                {
+                    waterTiles[waterTileFreezeRight].GetComponent<Water>().Frozen();
+                    waterTileFreezeRight++;
+                }
+
+                if (waterTileFreezeLeft < 0 && waterTileFreezeRight == waterLength)
+                {
+                    waterFreezeTimer = 0;
+                    isFreezing = false;
+                }
+            }
         }
-        if (spriteNumber == animationFrames)
+
+        if (isUnfreezing)
         {
-            spriteNumber = 0;
+            if (waterUnfreezeTimer < freezeSpeed)
+            {
+                waterUnfreezeTimer += Time.deltaTime;
+            }
+            else
+            {
+                waterUnfreezeTimer = 0;
+                if (waterTileUnfreezeLeft >= 0)
+                {
+                    waterTiles[waterTileUnfreezeLeft].GetComponent<Water>().Unfrozen();
+                    waterTileUnfreezeLeft--;
+                }
+                if (waterTileUnfreezeRight < waterLength)
+                {
+                    waterTiles[waterTileUnfreezeRight].GetComponent<Water>().Unfrozen();
+                    waterTileUnfreezeRight++;
+                }
+
+                if (waterTileUnfreezeLeft < 0 && waterTileUnfreezeRight == waterLength)
+                {
+                    waterUnfreezeTimer = 0;
+                    isUnfreezing = false;
+                }
+            }
         }
     }
 
-    public IEnumerator FreezeWater(int waterIdLeft, int waterIdRight)
+    //public IEnumerator FreezeWater(int waterIdLeft, int waterIdRight)
+    //{
+    //    if (!waterTiles[waterIdLeft].GetComponent<Water>().isFrozen)
+    //    {
+    //        yield return new WaitForSeconds(freezeSpeed);
+    //        waterTiles[waterIdLeft].GetComponent<Water>().isFrozen = true;
+
+    //        if (waterIdLeft > 0)
+    //        {
+    //            waterIdLeft -= 1;
+    //            StartCoroutine(FreezeWater(waterIdLeft, waterIdLeft));
+    //        }
+
+    //        if (waterIdRight < waterTiles.Count - 1)
+    //        {
+    //            waterIdRight += 1;
+    //            StartCoroutine(FreezeWater(waterIdLeft, waterIdLeft));
+    //        }
+    //    }
+
+    //    if (!waterTiles[waterIdRight].GetComponent<Water>().isFrozen)
+    //    {
+    //        yield return new WaitForSeconds(freezeSpeed);
+    //        waterTiles[waterIdRight].GetComponent<Water>().isFrozen = true;
+
+    //        if (waterIdLeft > 0)
+    //        {
+    //            waterIdLeft -= 1;
+    //            StartCoroutine(FreezeWater(waterIdRight, waterIdRight));
+    //        }
+
+    //        if (waterIdRight < waterTiles.Count - 1)
+    //        {
+    //            waterIdRight += 1;
+    //            StartCoroutine(FreezeWater(waterIdRight, waterIdRight));
+    //        }
+    //    }
+    //}
+
+    //public IEnumerator UnFreezeWater(int waterIdLeft, int waterIdRight)
+    //{
+    //    if (waterTiles[waterIdLeft].GetComponent<Water>().isFrozen)
+    //    {
+    //        yield return new WaitForSeconds(freezeSpeed);
+    //        waterTiles[waterIdLeft].GetComponent<Water>().isFrozen = false;
+    //        waterTiles[waterIdLeft].GetComponent<Water>().unfreeze = false;
+    //        waterTiles[waterIdLeft].GetComponent<Water>().unfreezeTimer = 0;
+    //        waterTiles[waterIdLeft].GetComponent<Water>().unfreezeNumber = 0;
+
+    //        if (waterIdLeft > 0)
+    //        {
+    //            waterIdLeft -= 1;
+    //            StartCoroutine(UnFreezeWater(waterIdLeft, waterIdLeft));
+    //        }
+
+    //        if (waterIdRight < waterTiles.Count - 1)
+    //        {
+    //            waterIdRight += 1;
+    //            StartCoroutine(UnFreezeWater(waterIdLeft, waterIdLeft));
+    //        }
+    //    }
+
+    //    if (waterTiles[waterIdRight].GetComponent<Water>().isFrozen)
+    //    {
+    //        yield return new WaitForSeconds(freezeSpeed);
+    //        waterTiles[waterIdRight].GetComponent<Water>().isFrozen = false;
+    //        waterTiles[waterIdRight].GetComponent<Water>().unfreeze = false;
+    //        waterTiles[waterIdLeft].GetComponent<Water>().unfreezeTimer = 0;
+    //        waterTiles[waterIdLeft].GetComponent<Water>().unfreezeNumber = 0;
+
+    //        if (waterIdLeft > 0)
+    //        {
+    //            waterIdLeft -= 1;
+    //            StartCoroutine(UnFreezeWater(waterIdRight, waterIdRight));
+    //        }
+
+    //        if (waterIdRight < waterTiles.Count - 1)
+    //        {
+    //            waterIdRight += 1;
+    //            StartCoroutine(UnFreezeWater(waterIdRight, waterIdRight));
+    //        }
+    //    }
+    //}
+
+    public void FreezeWater(int waterId, bool freezing)
     {
-        if (!waterTiles[waterIdLeft].GetComponent<Water>().isFrozen)
-        {
-            yield return new WaitForSeconds(freezeSpeed);
-            waterTiles[waterIdLeft].GetComponent<Water>().isFrozen = true;
+        waterTileFreezeLeft = waterId - 1;
+        waterTileFreezeRight = waterId + 1;
 
-            if (waterIdLeft > 0)
-            {
-                waterIdLeft -= 1;
-                StartCoroutine(FreezeWater(waterIdLeft, waterIdLeft));
-            }
+        waterTiles[waterId].GetComponent<Water>().Frozen();
 
-            if (waterIdRight < waterTiles.Count - 1)
-            {
-                waterIdRight += 1;
-                StartCoroutine(FreezeWater(waterIdLeft, waterIdLeft));
-            }
-        }
-
-        if (!waterTiles[waterIdRight].GetComponent<Water>().isFrozen)
-        {
-            yield return new WaitForSeconds(freezeSpeed);
-            waterTiles[waterIdRight].GetComponent<Water>().isFrozen = true;
-
-            if (waterIdLeft > 0)
-            {
-                waterIdLeft -= 1;
-                StartCoroutine(FreezeWater(waterIdRight, waterIdRight));
-            }
-
-            if (waterIdRight < waterTiles.Count - 1)
-            {
-                waterIdRight += 1;
-                StartCoroutine(FreezeWater(waterIdRight, waterIdRight));
-            }
-        }
+        isFreezing = freezing;
     }
 
-    public IEnumerator UnFreezeWater(int waterIdLeft, int waterIdRight)
+    public void UnfreezeWater(int waterId, bool unfreezing)
     {
-        if (waterTiles[waterIdLeft].GetComponent<Water>().isFrozen)
-        {
-            yield return new WaitForSeconds(freezeSpeed);
-            waterTiles[waterIdLeft].GetComponent<Water>().isFrozen = false;
-            waterTiles[waterIdLeft].GetComponent<Water>().unfreeze = false;
-            waterTiles[waterIdLeft].GetComponent<Water>().unfreezeTimer = 0;
-            waterTiles[waterIdLeft].GetComponent<Water>().unfreezeNumber = 0;
+        waterTileUnfreezeLeft = waterId - 1;
+        waterTileUnfreezeRight = waterId + 1;
 
-            if (waterIdLeft > 0)
-            {
-                waterIdLeft -= 1;
-                StartCoroutine(UnFreezeWater(waterIdLeft, waterIdLeft));
-            }
+        waterTiles[waterId].GetComponent<Water>().Unfrozen();
 
-            if (waterIdRight < waterTiles.Count - 1)
-            {
-                waterIdRight += 1;
-                StartCoroutine(UnFreezeWater(waterIdLeft, waterIdLeft));
-            }
-        }
-
-        if (waterTiles[waterIdRight].GetComponent<Water>().isFrozen)
-        {
-            yield return new WaitForSeconds(freezeSpeed);
-            waterTiles[waterIdRight].GetComponent<Water>().isFrozen = false;
-            waterTiles[waterIdRight].GetComponent<Water>().unfreeze = false;
-            waterTiles[waterIdLeft].GetComponent<Water>().unfreezeTimer = 0;
-            waterTiles[waterIdLeft].GetComponent<Water>().unfreezeNumber = 0;
-
-            if (waterIdLeft > 0)
-            {
-                waterIdLeft -= 1;
-                StartCoroutine(UnFreezeWater(waterIdRight, waterIdRight));
-            }
-
-            if (waterIdRight < waterTiles.Count - 1)
-            {
-                waterIdRight += 1;
-                StartCoroutine(UnFreezeWater(waterIdRight, waterIdRight));
-            }
-        }
+        isUnfreezing = unfreezing;
     }
 }
--- GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/Trampoline.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/Trampoline.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
+    public enum WhoCanJump { Night, Day, Both }
+
+    public WhoCanJump setJumper;
+
     public float jumpForce;
+
+    private Color nightColor, dayColor, bothColor;
+
+    private SpriteRenderer theSR;
+
+    private float playerJumpForce;
+
+    private int startLayer; 
+
+    private void Start()
+    {
+        theSR = GetComponentInChildren<SpriteRenderer>();
+        nightColor = Color.blue;
+        dayColor = Color.yellow;
+        bothColor = Color.green;
+
+        playerJumpForce = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementTest>().jumpForce;
+
+        startLayer = gameObject.layer;
+
+        if (setJumper.ToString() == "Both")
+        {
+            theSR.color = bothColor;
+        }
+        if (setJumper.ToString() == "Night")
+        {
+            theSR.color = nightColor;
+        }
+        if (setJumper.ToString() == "Day")
+        {
+            theSR.color = dayColor;
+        }
+    }
 
     //private void OnTriggerEnter2D(Collider2D other)
     //{
@@ -25,8 +62,37 @@ public class Trampoline : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            other.gameObject.GetComponent<PlayerMovementTest>().theRB.velocity = Vector2.zero;
-            other.gameObject.GetComponent<PlayerMovementTest>().theRB.AddForce(new Vector2(0f, jumpForce));
+            if (setJumper.ToString() == "Both")
+            {
+                other.gameObject.GetComponent<PlayerMovementTest>().theRB.velocity = Vector2.zero;
+                other.gameObject.GetComponent<PlayerMovementTest>().theRB.AddForce(new Vector2(0f, jumpForce));
+            }
+
+            if (setJumper.ToString() == "Night")
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+                {
+                    other.gameObject.GetComponent<PlayerMovementTest>().theRB.velocity = Vector2.zero;
+                    other.gameObject.GetComponent<PlayerMovementTest>().theRB.AddForce(new Vector2(0f, jumpForce));
+                }
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+                {
+                    gameObject.layer = default;
+                }
+            }
+
+            if (setJumper.ToString() == "Day")
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+                {
+                    other.gameObject.GetComponent<PlayerMovementTest>().theRB.velocity = Vector2.zero;
+                    other.gameObject.GetComponent<PlayerMovementTest>().theRB.AddForce(new Vector2(0f, jumpForce));
+                }
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+                {
+                    gameObject.layer = default;
+                }
+            }
 
             if (other.gameObject.GetComponent<PlayerMovementTest>().canDoubleJump)
             {
@@ -38,6 +104,33 @@ public class Trampoline : MonoBehaviour
         {
             //other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce * 5));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (setJumper.ToString() == "Night")
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+                {
+                    gameObject.layer = startLayer;
+                }
+            }
+
+            if (setJumper.ToString() == "Day")
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+                {
+                    gameObject.layer = startLayer;
+                }
+            }
+
+            if (other.gameObject.GetComponent<PlayerMovementTest>().canDoubleJump)
+            {
+                other.gameObject.GetComponent<PlayerMovementTest>().doubleJumpCounter = 1;
+            }
         }
     }
 }
