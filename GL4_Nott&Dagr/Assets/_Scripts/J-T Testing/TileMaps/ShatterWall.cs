fe2e8771543177a82870f729f9febf21d857f2cd
Xzendor <jtors2@hotmail.com>
2020-02-21 09:08:07 +0100

    Wall shatter prefab.

---
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/ShatterWall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterWall : MonoBehaviour
+{
+    public int wallHeight;
+
+    public GameObject wallPrefab;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = null;
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        for (int i = 0; i < wallHeight; i++)
+        {
+            GameObject wall = Instantiate(wallPrefab, transform.position + new Vector3(0f, i, 0f), transform.rotation);
+        }
+    }
+}
---
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/Wall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+            transform.GetChild(i).GetComponent<CircleCollider2D>().enabled = true;
+            Destroy(transform.GetChild(i).gameObject, 5f);
+        }
+        transform.DetachChildren();
+        Destroy(gameObject);
+    }
+}
