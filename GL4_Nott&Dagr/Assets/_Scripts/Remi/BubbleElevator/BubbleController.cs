e7bfd4ab1e3996fd502dfa85e46f93f2c2d0b221
froztyseven <froztyseven@hotmail.com>
2020-02-21 10:12:53 +0100

    Added bubble speed modifiers

--- GL4_Nott&Dagr/Assets/_Scripts/Remi/BubbleElevator/BubbleController.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/Remi/BubbleElevator/BubbleController.cs
@@ -8,82 +8,83 @@ using UnityEngine.UIElements;
 [SelectionBase]
 public class BubbleController : MonoBehaviour
 {
-    public enum BubbleMovement
-    {
-        Disabled,
-        OnceToDestination,
-        AlwaysToDestination
-    };
-    
-    [Header("Bubble Settings")]
-    public BubbleMovement movement;
-    
-    
-    [Range(0.1f, 10.0f)]
-    public float speed = 0.4f;
 
+    public enum SpeedModifier
+    {
+        ConsistentSpeed,
+        SplitSpeed
+    };
+
+
+    [Header("Bubble Settings")]
+    public SpeedModifier pathSpeedModifier;
+
+
+    [Range(0f, 10.0f)] public float consistentSpeed = 0.4f;
+
+    [Space(20)] [Range(0f, 10.0f)] public float splitForwardSpeed = 0.4f;
+    [Range(0f, 10.0f)] public float splitBackwardsSpeed = 0.4f;
     
-    [HideInInspector]
-    public int bubbleMoveInt = 0;
+        
+    [HideInInspector] public int bubbleMoveInt = 0;
 
     private Vector3 posA;
     private Vector3 posB;
 
 
-    [Header("Movement Path Setup")]
-    [SerializeField] 
-    private Transform currentBubble;
-    [SerializeField] 
-    private Transform movePathChild;
+    [Header("Movement Path Setup")] [SerializeField]
+    Transform currentBubble;
+
+    [SerializeField] private Transform movePathChild;
 
     
     private Vector3 startPos;
     private Vector3 endPos;
-    
+
     GameObject _player1;
     GameObject _player2;
     private Component _parentScript;
+    
+    
+    private BoxCollider2D childCollider;
 
     private void Start()
     {
+        childCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+        
         _parentScript = GetComponentInParent(typeof(BubblePath));
         _player1 = GameObject.Find("Player1");
         _player2 = GameObject.Find("Player2");
-        
+
         posA = currentBubble.localPosition;
         posB = movePathChild.localPosition;
         endPos = posB;
         startPos = transform.position;
     }
-    
+
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
             {
-                if (movement.ToString() == "AlwaysToDestination" || movement.ToString() == "OnceToDestination")
+                _player2.transform.parent = this.transform;
+
+                if (bubbleMoveInt == 0)
                 {
-                    _player2.transform.parent = this.transform;
-                    
-                    if (bubbleMoveInt == 0)
-                    {
-                        bubbleMoveInt = 1;
-                    }
+                    bubbleMoveInt = 1;
                 }
             }
         }
-        
+
         if (other.gameObject.CompareTag("Player"))
         {
             if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
             {
-                if (movement.ToString() == "AlwaysToDestination" || movement.ToString() == "OnceToDestination")
-                {
-                    _player1.transform.parent = this.transform;
-                    GetComponent<SpriteRenderer>().color = Color.yellow;
-                    bubbleMoveInt = 2;
-                }
+                _player1.transform.parent = this.transform;
+                GetComponent<SpriteRenderer>().color = Color.yellow;
+                bubbleMoveInt = 2;
             }
         }
     }
@@ -94,29 +95,22 @@ public class BubbleController : MonoBehaviour
         {
             if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
             {
-                if (movement.ToString() == "AlwaysToDestination" || movement.ToString() == "OnceToDestination")
-                {
-                    _player2.transform.parent = null;
-                    
-                    bubbleMoveInt = 0;
-                }
+                _player2.transform.parent = null;
+
+                bubbleMoveInt = 0;
             }
         }
-        
+
         if (other.gameObject.CompareTag("Player"))
         {
             if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
             {
-                if (movement.ToString() == "AlwaysToDestination" || movement.ToString() == "OnceToDestination")
-                {
-                    _player1.transform.parent = null;
-                    bubbleMoveInt = 0;
-                    GetComponent<SpriteRenderer>().color = Color.white;
-                }
+                _player1.transform.parent = null;
+                bubbleMoveInt = 0;
+                GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
     }
-
 
 
     private void Update()
@@ -124,28 +118,40 @@ public class BubbleController : MonoBehaviour
         // Bubble return to default path when no players are inside
         if (bubbleMoveInt == 0)
         {
-            if (movement.ToString() == "Disabled")
-            {
-                Destroy(_parentScript);
-                speed = 0f;
-            }
-            if (movement.ToString() == "AlwaysToDestination")
+            if (pathSpeedModifier.ToString() == "ConsistentSpeed")
             {
                 currentBubble.localPosition =
-                    Vector3.MoveTowards(currentBubble.localPosition, posA, speed * Time.deltaTime);
+                    Vector3.MoveTowards(currentBubble.localPosition, posA, consistentSpeed * Time.deltaTime);
+            }
+            else if (pathSpeedModifier.ToString() == "SplitSpeed")
+            {
+                currentBubble.localPosition =
+                    Vector3.MoveTowards(currentBubble.localPosition, posA, splitBackwardsSpeed * Time.deltaTime);
             }
         }
 
-
         // bubbleMoveInt == 1 is set to player "Night" and will freeze the bubble to place
+        if (bubbleMoveInt > 0)
+        {
+            childCollider.enabled = true;
+        }
+        else
+        {
+            childCollider.enabled = false;
+        }
 
         // Bubble starts moving when player "Day" is inside
         if (bubbleMoveInt == 2)
         {
-            if (movement.ToString() == "AlwaysToDestination" || movement.ToString() == "OnceToDestination")
+            if (pathSpeedModifier.ToString() == "ConsistentSpeed")
             {
                 currentBubble.localPosition =
-                    Vector3.MoveTowards(currentBubble.localPosition, endPos, speed * Time.deltaTime);
+                    Vector3.MoveTowards(currentBubble.localPosition, endPos, consistentSpeed * Time.deltaTime);
+            }
+            else if (pathSpeedModifier.ToString() == "SplitSpeed")
+            {
+                currentBubble.localPosition =
+                    Vector3.MoveTowards(currentBubble.localPosition, endPos, splitForwardSpeed * Time.deltaTime);
             }
         }
     }
