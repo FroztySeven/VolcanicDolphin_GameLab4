9394da3196814a12595f3e7982290c29778362d0
Xzendor <jtors2@hotmail.com>
2020-02-25 08:59:58 +0100

    Melting ice cube.

---
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/TileMaps/IceCube.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCube : MonoBehaviour
+{
+    public int unfreezeDuration;
+
+    private bool unfreeze;
+    private float unfreezeTimer = 0;
+    private int unfreezeNumber = 0;
+    private SpriteRenderer theSR;
+
+    public Color unfreezeColor;
+    private Color startColor;
+
+    private void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+        startColor = theSR.color;    
+    }
+
+    private void Update()
+    {
+        if (unfreeze)
+        {
+            float freezeSpeed = 4;
+
+            if (unfreezeTimer < 1)
+            {
+                unfreezeTimer += Time.deltaTime * freezeSpeed;
+            }
+            if (unfreezeTimer >= 1)
+            {
+                unfreezeNumber++;
+                unfreezeTimer = 0;
+            }
+            if (unfreezeNumber % 2 == 0)
+            {
+                theSR.color = startColor;
+            }
+            else
+            {
+                theSR.color = unfreezeColor;
+            }
+            if (unfreezeNumber == unfreezeDuration * 2)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                unfreeze = true;
+            }
+        }
+    }
+}
