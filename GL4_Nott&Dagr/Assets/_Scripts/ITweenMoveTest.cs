36ef1a750fbfa11e6935ac05f33d9d28ba013596
Xzendor <jtors2@hotmail.com>
2020-03-06 10:05:40 +0100

    iTween update.

---
+++ GL4_Nott&Dagr/Assets/_Scripts/ITweenMoveTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ITweenMoveTest : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float speed;
+    private float[] velocityCheck = new float[2];
+    private Rigidbody2D theRB;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        theRB = GetComponent<Rigidbody2D>();
+        velocityCheck[0] = 0f;
+        velocityCheck[1] = 0f;
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        iTween.Stop();
+    //        transform.position = startPos;
+    //        PathOne();
+    //    }
+    //}
+
+    private void FixedUpdate()
+    {
+        velocityCheck[1] = velocityCheck[0];
+        velocityCheck[0] = theRB.position.x;
+
+        //speed = velocityCheck[1] - velocityCheck[0];
+    }
+
+    private void PathOne()
+    {
+        TurnOffRigidbody();
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("TestPath"), "time", 1, "easetype", iTween.EaseType.easeInSine, "oncomplete", "PathTwo"));
+    }
+
+    private void PathTwo()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("TestPath2"), "time", 1, "easetype", iTween.EaseType.easeOutSine, "oncomplete", "TurnOnRigidbody"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "StartBounce")
+        {
+            PathOne();
+        }
+    }
+
+    private void TurnOffRigidbody()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    private void TurnOnRigidbody()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+}
