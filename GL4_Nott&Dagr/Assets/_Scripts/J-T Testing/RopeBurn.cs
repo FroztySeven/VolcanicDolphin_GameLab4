7667ac8dda9e64c2abc37a1be9d891d6457387af
sindrebrokke <sindre980@hotmail.com>
2020-03-09 09:27:10 +0100

    Platform effector fix

--- GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/RopeBurn.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/RopeBurn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RopeBurn : MonoBehaviour
 {
+    public GameObject platform;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
@@ -11,6 +12,8 @@ public class RopeBurn : MonoBehaviour
             if (other.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
             {
                 gameObject.SetActive(false);
+                platform.GetComponent<PlatformEffector2D>().enabled = false;
+                platform.GetComponent<BoxCollider2D>().usedByEffector = false;
             }
         }
     }
