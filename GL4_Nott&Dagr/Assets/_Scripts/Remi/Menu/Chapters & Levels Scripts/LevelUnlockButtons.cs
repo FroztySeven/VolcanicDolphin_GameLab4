a67f099f7d58291af52496eb2d3be78951adab9c
FroztySeven <froztyseven@hotmail.com>
2020-03-16 14:25:15 +0100

    Added cutscenes between chapters

--- GL4_Nott&Dagr/Assets/_Scripts/Remi/Menu/Chapters & Levels Scripts/LevelUnlockButtons.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/Remi/Menu/Chapters & Levels Scripts/LevelUnlockButtons.cs
@@ -36,16 +36,17 @@ public class LevelUnlockButtons : MonoBehaviour
     [Space(20)]
     public Button[] C9;
 
-    
+    public Text progressPrefsText;
 
     void Start()
     {
-        int levelReached = PlayerPrefs.GetInt("LevelPrefs", 2);
+        int levelReached = PlayerPrefs.GetInt("LevelPrefs", 3);
+        progressPrefsText.text = "Unlock prefs int: " + levelReached.ToString();
 
         //Disable all chapter 1 buttons except the first one, where the PlayerPrefs integer is bigger than "2"
         for (int i = 0; i < C1.Length; i++)
         {
-            if (i + 2 > levelReached)
+            if (i + 3 > levelReached)
             {
                 C1[i].interactable = false;
             }
@@ -53,7 +54,11 @@ public class LevelUnlockButtons : MonoBehaviour
         //Disable all chapter 2 buttons except the first one, where the PlayerPrefs integer is bigger than "6"
         for (int i = 0; i < C2.Length; i++)
         {
-            if (i + 6 > levelReached)
+            if (i + 7 > levelReached)
+            {
+                C2[0].interactable = true;
+            }
+            if (i + 8 > levelReached)
             {
                 C2[i].interactable = false;
             }
@@ -62,7 +67,11 @@ public class LevelUnlockButtons : MonoBehaviour
         //Disable all chapter 2 buttons except the first one, where the PlayerPrefs integer is bigger than "6"
         for (int i = 0; i < C3.Length; i++)
         {
-            if (i + 10 > levelReached)
+            if (i + 12 > levelReached)
+            {
+                C3[0].interactable = true;
+            }
+            if (i + 13 > levelReached)
             {
                 C3[i].interactable = false;
             }
@@ -72,11 +81,11 @@ public class LevelUnlockButtons : MonoBehaviour
     void Update()
     {
         //When the integer is bigger than 5, unlock chapter button 2
-        if (PlayerPrefs.GetInt("LevelPrefs") > 5)
+        if (PlayerPrefs.GetInt("LevelPrefs") > 6)
         {
             allChapterButtons[1].gameObject.SetActive(true);
         }
-        if (PlayerPrefs.GetInt("LevelPrefs") > 9)
+        if (PlayerPrefs.GetInt("LevelPrefs") > 11)
         {
             allChapterButtons[2].gameObject.SetActive(true);
         }
