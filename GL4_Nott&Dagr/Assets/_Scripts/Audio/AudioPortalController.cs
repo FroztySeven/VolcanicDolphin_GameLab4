7c0a2a10c3532854b24a51aa64c5226722c1ec68
obrannug <gunnarbodvars@gmail.com>
2020-03-17 18:40:54 +0100

    Fixed door chime when portal opens

---
+++ GL4_Nott&Dagr/Assets/_Scripts/Audio/AudioPortalController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPortalController : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string portalChime;
+
+    public GameObject doorPortal;
+
+    public bool portalActive, playChime;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        doorPortal = GameObject.Find("PortalSwirl");
+    }
+
+    void Start()
+    {
+        playChime = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorPortal.activeSelf == true)
+        {
+            portalActive = true;
+        }
+
+        if (portalActive == true && playChime == true)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(portalChime);
+            playChime = false;
+        }
+    }
+}
