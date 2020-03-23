fe6a5f6608727b09c3933fb8ced55064c2de2873
sindrebr <sindre980@hotmail.com>
2020-03-23 15:10:48 +0100

    Added a swinging rope

---
+++ GL4_Nott&Dagr/Assets/_Scripts/SindreTest/SwingingRope.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingingRope : MonoBehaviour
+{
+    public bool nott, dagr, both;
+
+    public int ropeLength;
+
+    public Sprite ropeSprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ropeLength = ropeLength - 1;
+        
+        for (int i = 0; i < ropeLength; i++) //spawning of the rope depending on how long it should be.
+        {
+            GameObject sprite = new GameObject("Rope" + (i + 1));
+            sprite.transform.parent = transform;
+            sprite.transform.position = transform.position + new Vector3(0f, -i - 1f, 0f);
+            sprite.AddComponent<SpriteRenderer>().sprite = ropeSprite;
+            sprite.AddComponent<BoxCollider2D>();
+            sprite.AddComponent<HingeJoint2D>().connectedBody = this.gameObject.transform.GetChild(i).GetComponent<Rigidbody2D>();
+            sprite.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+            sprite.GetComponent<Rigidbody2D>().mass = 25;
+            sprite.layer = 11;
+
+            if (i == ropeLength -1) //spawning an empty gameobject with a trigger and another script on it.
+            {
+                GameObject endZone = new GameObject("EndZone");
+                endZone.transform.parent = gameObject.transform.GetChild(i + 1);
+                endZone.transform.position = transform.position + new Vector3(0f, -i - 1, 0f);
+                endZone.AddComponent<BoxCollider2D>().isTrigger = true;
+                endZone.GetComponent<BoxCollider2D>().size = new Vector2(0.25f, 1);
+                endZone.AddComponent<SwingingRopeEndZone>();
+            }
+        }
+    }
+}
---
+++ GL4_Nott&Dagr/Assets/_Scripts/SindreTest/SwingingRopeEndZone.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class SwingingRopeEndZone : MonoBehaviour
+{
+    private SwingingRope _sr;
+
+    private GameObject dagrGO, nottGO;
+
+    private bool dagrOnRope = false, nottOnRope = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dagrGO = GameObject.Find("Player1");
+        nottGO = GameObject.Find("Player2");
+        
+        _sr = gameObject.GetComponentInParent<SwingingRope>();
+    }
+
+
+    private void Update()
+    {
+        if (dagrOnRope)
+        {
+            if (_sr.dagr || _sr.both)
+            {
+                dagrGO.transform.SetParent(this.transform);
+                dagrGO.transform.position = transform.position;
+                
+                if (Input.GetButtonDown("JumpP" + dagrGO.GetComponent<PlayerMovementTest>().playerId))
+                {
+                    dagrOnRope = false;
+                    dagrGO.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+                    gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    dagrGO.transform.SetParent(null);
+                    StartCoroutine(turnOnTriggerAgain());
+                }
+            }
+        }
+
+        if (nottOnRope)
+        {
+            if (_sr.nott || _sr.both)
+            {
+                nottGO.transform.SetParent(this.transform);
+                nottGO.transform.position = transform.position;
+                
+                if (Input.GetButtonDown("JumpP" + nottGO.GetComponent<PlayerMovementTest>().playerId))
+                {
+                    nottOnRope = false;
+                    nottGO.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+                    gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    nottGO.transform.SetParent(null);
+                    StartCoroutine(turnOnTriggerAgain());
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                dagrOnRope = true;
+            }
+
+            if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+            {
+                nottOnRope = true;
+            }
+        }
+    }
+
+    /*private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (_sr.dagr)
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+                {
+                    other.gameObject.transform.SetParent(this.transform);
+                    other.transform.position = this.transform.position;
+
+                    if (Input.GetButtonDown("JumpP" + other.gameObject.GetComponent<PlayerMovementTest>().playerId))
+                    {
+                        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                        other.transform.SetParent(null);
+                        StartCoroutine(turnOnTriggerAgain());
+                    }
+                }
+            }
+            if (_sr.nott)
+            {
+                if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+                {
+                    other.gameObject.transform.SetParent(this.transform);
+                    other.transform.position = this.transform.position;
+
+                    if (Input.GetButtonDown("JumpP" + other.gameObject.GetComponent<PlayerMovementTest>().playerId))
+                    {
+                        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                        other.transform.SetParent(null);
+                        StartCoroutine(turnOnTriggerAgain());
+                    }
+                }
+            }
+            
+            if (_sr.both)
+            { 
+                other.gameObject.transform.SetParent(this.transform);
+                other.transform.position = transform.position;
+
+                if (Input.GetButtonDown("JumpP" + other.gameObject.GetComponent<PlayerMovementTest>().playerId)) 
+                {
+                    gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    other.transform.SetParent(null);
+                    StartCoroutine(turnOnTriggerAgain());
+                } 
+            }
+        }
+    }*/
+
+
+    /*private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(null);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(turnOnTriggerAgain());
+
+            if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Day")
+            {
+                dagrOnRope = false;
+            }
+            if (other.gameObject.GetComponent<PlayerMovementTest>().setPlayer.ToString() == "Night")
+            {
+                nottOnRope = false;
+            }
+        }
+    }*/
+
+    private IEnumerator turnOnTriggerAgain()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
