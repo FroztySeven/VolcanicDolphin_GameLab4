d6a856e963e8ee4019c8a44cb103f882c2220557
obrannug <gunnarbodvars@gmail.com>
2020-04-22 18:43:08 +0200

    New level for vanaheim, still in testing, it is in my scene folder. Added Pre_TrickWall that has multple settings on how it behaves. Also added a rigidbody for minion so it could push my button.

---
+++ GL4_Nott&Dagr/Assets/_Scripts/Audio/InTesting/ReverseWallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseWallController : MonoBehaviour
+{
+    //... Notice! This script was originally made by Jan-Tore.
+    //... Original scripts used were PressurePlates and PressurePlatesWall. 
+    //... I (Gunnar), made a copy those scripts to make alterations to them, add more variants of wall behaviours that I wanted.
+
+    public int wallHeight = 1;
+    public int wallWidth = 1;
+
+    public Sprite wallSprite;
+
+    public GameObject wallPrefab;
+
+    public bool showMe;
+
+    private void Start()
+    {
+        for (int i = 0; i < wallWidth; i++)
+        {
+            for (int j = 0; j < wallHeight; j++)
+            {
+                GameObject wall = Instantiate(wallPrefab, transform.position + new Vector3(i, j, 0f), transform.rotation);
+                wall.GetComponent<SpriteRenderer>().sprite = wallSprite;
+                wall.transform.parent = transform;
+            }
+        }
+
+        Destroy(wallPrefab);
+    }
+}
