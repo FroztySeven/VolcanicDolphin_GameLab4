8773a4b548108c671f01043c58e210a479a68edc
Xzendor <jtors2@hotmail.com>
2020-05-25 10:09:26 +0200

    Split screen particle, and added background particle for Muspelheim_10.

--- GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/SplitScreen.cs
+++ GL4_Nott&Dagr/Assets/_Scripts/J-T Testing/SplitScreen.cs
@@ -23,8 +23,31 @@ public class SplitScreen : MonoBehaviour {
 	//The two quads used to draw the second screen, both of which are initalized in the start function.
 	private GameObject split;
 	private GameObject splitter;
+	private GameObject splitter2;
 
-	void Start () {
+    [SerializeField]
+    private GameObject splitParticle;
+    [SerializeField]
+    private GameObject splitParticle2;
+    [SerializeField]
+    private GameObject splitParticle3;
+
+    private int numberOfParticleSystems;
+    private ParticleSystem[] particles;
+
+    void Start () {
+
+        numberOfParticleSystems = splitParticle.transform.childCount + splitParticle2.transform.childCount + splitParticle3.transform.childCount;
+
+        particles = new ParticleSystem[numberOfParticleSystems];
+
+        particles[0] = splitParticle.transform.GetChild(0).transform.GetComponent<ParticleSystem>();
+        particles[1] = splitParticle.transform.GetChild(1).transform.GetComponent<ParticleSystem>();
+        particles[2] = splitParticle2.transform.GetChild(0).transform.GetComponent<ParticleSystem>();
+        particles[3] = splitParticle2.transform.GetChild(1).transform.GetComponent<ParticleSystem>();
+        particles[4] = splitParticle3.transform.GetChild(0).transform.GetComponent<ParticleSystem>();
+        particles[5] = splitParticle3.transform.GetChild(1).transform.GetComponent<ParticleSystem>();
+
 		//Referencing camera1 and initalizing camera2.
 		camera1 = Camera.main.gameObject;
 		camera2 = new GameObject ();
@@ -35,16 +58,34 @@ public class SplitScreen : MonoBehaviour {
 		//Setting up the culling mask of camera2 to ignore the layer "TransparentFX" as to avoid rendering the split and splitter on both cameras.
 		camera2.GetComponent<Camera> ().cullingMask = ~(1 << LayerMask.NameToLayer ("TransparentFX"));
 
-		//Setting up the splitter and initalizing the gameobject.
-		splitter = GameObject.CreatePrimitive (PrimitiveType.Quad);
+        //Setting up the splitter and initalizing the gameobject.
+        splitter = GameObject.CreatePrimitive (PrimitiveType.Quad);
 		splitter.transform.parent = gameObject.transform;
-		splitter.transform.localPosition = Vector3.forward;
-		splitter.transform.localScale = new Vector3 (50, splitterWidth/10, 1);
+        splitter.transform.localPosition = new Vector3(0f, 0f, 5f);
+        splitter.transform.localScale = new Vector3 (50, splitterWidth/10, 1);
 		splitter.transform.localEulerAngles = Vector3.zero;
-		splitter.SetActive (false);
+        splitter.GetComponent<MeshRenderer>().enabled = false;
+        splitter.SetActive (false);
 
-		//Setting up the split and initalizing the gameobject.
-		split = GameObject.CreatePrimitive (PrimitiveType.Quad);
+        splitParticle.transform.parent = splitter.transform;
+        splitParticle.transform.localPosition = new Vector3(0f, 0f, -2.5f);
+
+        splitParticle3.transform.parent = splitter.transform;
+        splitParticle3.transform.localPosition = new Vector3(0f, 0f, -2.5f);
+
+        splitter2 = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        splitter2.transform.parent = camera2.transform;
+        splitter2.transform.localPosition = new Vector3(0f, 0f, 5f);
+        splitter2.transform.localScale = new Vector3(50, splitterWidth / 10, 1);
+        splitter2.transform.localEulerAngles = Vector3.zero;
+        splitter2.GetComponent<MeshRenderer>().enabled = false;
+
+        splitParticle2.transform.parent = splitter2.transform;
+        splitParticle2.transform.localPosition = new Vector3(0f, 0f, 2.5f);
+
+
+        //Setting up the split and initalizing the gameobject.
+        split = GameObject.CreatePrimitive (PrimitiveType.Quad);
 		split.transform.parent = splitter.transform;
 		split.transform.localPosition = new Vector3 (0,-(15/(splitterWidth/10)),0);
 		split.transform.localScale = new Vector3 (1, 30/(splitterWidth/10), 1);
@@ -56,7 +97,10 @@ public class SplitScreen : MonoBehaviour {
 		splitter.GetComponent<Renderer>().material = tempMat;
 		splitter.GetComponent<Renderer> ().sortingOrder = 2;
 		splitter.layer = LayerMask.NameToLayer ("TransparentFX");
-		Material tempMat2 = new Material (Shader.Find ("Mask/SplitScreen"));
+        splitter2.GetComponent<Renderer>().material = tempMat;
+        splitter2.GetComponent<Renderer>().sortingOrder = 2;
+        splitter2.layer = LayerMask.NameToLayer("TransparentFX");
+        Material tempMat2 = new Material (Shader.Find ("Mask/SplitScreen"));
 		split.GetComponent<Renderer>().material = tempMat2;
 		split.layer = LayerMask.NameToLayer ("TransparentFX");
 	}
@@ -76,9 +120,10 @@ public class SplitScreen : MonoBehaviour {
 
 		//Rotates the splitter according to the new angle.
 		splitter.transform.localEulerAngles = new Vector3 (0, 0, angle);
+		splitter2.transform.localEulerAngles = new Vector3 (0, 0, angle);
 
-		//Gets the exact midpoint between the two players.
-		Vector3 midPoint = new Vector3 ((player1.position.x + player2.position.x) / 2, (player1.position.y + player2.position.y) / 2, (player1.position.z + player2.position.z) / 2); 
+        //Gets the exact midpoint between the two players.
+        Vector3 midPoint = new Vector3 ((player1.position.x + player2.position.x) / 2, (player1.position.y + player2.position.y) / 2, (player1.position.z + player2.position.z) / 2); 
 
 		//Waits for the two cameras to split and then calcuates a midpoint relevant to the difference in position between the two cameras.
         if (yDistance > splitDistance / 2 || yDistance < -splitDistance / 2)
@@ -105,6 +150,7 @@ public class SplitScreen : MonoBehaviour {
                 camera2.transform.position = camera1.transform.position;
                 camera2.transform.rotation = camera1.transform.rotation;
 
+                Invoke("PlayParticles", 0.05f);
             }
             else
             {
@@ -138,6 +184,7 @@ public class SplitScreen : MonoBehaviour {
                 camera2.transform.position = camera1.transform.position;
                 camera2.transform.rotation = camera1.transform.rotation;
 
+                Invoke("PlayParticles", 0.05f);
             }
             else
             {
@@ -151,8 +198,11 @@ public class SplitScreen : MonoBehaviour {
         {
 			//Deactivates the splitter and camera once the distance is less than the splitting distance (assuming it was at one point).
 			if (splitter.activeSelf)
+            {
 				splitter.SetActive (false);
 				camera2.SetActive (false);
+                StopParticles();
+            }
 		}
 
 		/*Lerps the first cameras position and rotation to that of the second midpoint, so relative to the first player
@@ -160,5 +210,21 @@ public class SplitScreen : MonoBehaviour {
 		camera1.transform.position = Vector3.Lerp(camera1.transform.position,midPoint + new Vector3(0,0,-10),Time.deltaTime*5);
         //Quaternion newRot = Quaternion.LookRotation(midPoint - camera1.transform.position);
         //camera1.transform.rotation = Quaternion.Lerp(camera1.transform.rotation, newRot, Time.deltaTime * 5);
+    }
+
+    private void PlayParticles()
+    {
+        for (int i = 0; i < numberOfParticleSystems; i++)
+        {
+            particles[i].Play();
+        }
+    }
+
+    private void StopParticles()
+    {
+        for (int i = 0; i < numberOfParticleSystems; i++)
+        {
+            particles[i].Stop();
+        }
     }
 }
